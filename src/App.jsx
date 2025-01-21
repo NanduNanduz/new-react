562e439bdaf107d55ff8145584138d142e878c91
NanduNanduz <nandunsb99@gmail.com>
2025-01-21 15:27:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Register from './components/Register'
+
+//all the content in return should be in opening and closing tag 
+//replace className to classNameName
+//
+const App = () => {
+  return (
+    <>
+    <Register/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse ms-2" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <h1>Hello Home </h1>
+    </div>
+  );
+}
+
+export default Home;
